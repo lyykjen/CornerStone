8625db38ab678c1eae6e80c42459cdadc086941b
lvyayun <yy.lv@yuneec.com>
2017-06-21 05:51:17 -0400

    fix test_queue

---
+++ work_test/22_queue/build/CMakeFiles/test_queue.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/lvyayun/test/work_test/22_queue/queue.c" "/home/lvyayun/test/work_test/22_queue/build/CMakeFiles/test_queue.dir/queue.c.o"
+  "/home/lvyayun/test/work_test/22_queue/test_queue.c" "/home/lvyayun/test/work_test/22_queue/build/CMakeFiles/test_queue.dir/test_queue.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
