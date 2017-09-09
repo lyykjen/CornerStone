ae6ece4e20bf4e04eed11d67f313a8c261c86216
lvyayun <yy.lv@yuneec.com>
2017-09-09 11:15:01 +0800

    add lcm test deamon

---
+++ work_test/32_lcm/build/CMakeFiles/buftest-receiver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buftest-receiver.dir/buftest-receiver.c.o"
+  "buftest-receiver.pdb"
+  "buftest-receiver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/buftest-receiver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
